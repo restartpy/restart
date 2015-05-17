179926fb1294ce9d9393dca0f405518fd801a530
RussellLuo <luopeng.he@gmail.com>
2015-05-17 11:59:48 +0800

    Add tests

---
+++ tests/test_router.py
@@ -0,0 +1,68 @@
+from __future__ import absolute_import
+
+import pytest
+
+from restart import router
+from restart.resource import Resource
+from restart.config import config
+
+
+class TestRouter(object):
+
+    def teardown_method(self, method):
+        router.rules.clear()
+
+    def assert_rule(self, result, expected):
+        uri, methods, resource_cls = expected
+        assert result.uri == uri
+        assert result.methods == methods
+        assert result.handler.resource_cls == resource_cls
+
+    def test_add_rule(self):
+        class Dummy(Resource):
+            name = 'dummy'
+
+        router.add_rule(Dummy, '/dummy', 'dummy')
+        assert 'dummy' in router.rules
+
+        rule = router.rules['dummy']
+        self.assert_rule(rule, ('/dummy', config.ACTION_MAP.keys(), Dummy))
+
+    def test_add_rule_with_exsiting_endpoint(self):
+        class Dummy(Resource):
+            name = 'dummy'
+
+        router.add_rule(Dummy, '/dummy', 'dummy')
+
+        with pytest.raises(AssertionError):
+            router.add_rule(Dummy, '/dummy', 'dummy')
+
+    def test_route(self):
+        @router.route(methods=['GET'])
+        class Item(Resource):
+            name = 'item'
+
+        assert 'item' in router.rules
+
+        rule = router.rules['item']
+        self.assert_rule(rule, ('/item', ['GET'], Item))
+
+    def test_register(self):
+        @router.register
+        class User(Resource):
+            name = 'users'
+
+        assert 'users_list' in router.rules
+        assert 'users_item' in router.rules
+
+        list_rule = router.rules['users_list']
+        self.assert_rule(
+            list_rule,
+            ('/users', ['POST', 'GET', 'DELETE'], User)
+        )
+
+        item_rule = router.rules['users_item']
+        self.assert_rule(
+            item_rule,
+            ('/users/<pk>', ['GET', 'PUT', 'PATCH', 'DELETE'], User)
+        )
---
+++ tests/test_utils.py
@@ -0,0 +1,74 @@
+from __future__ import absolute_import
+
+import os
+import sys
+import shutil
+import tempfile
+
+import pytest
+
+from restart.utils import load_resources, expand_wildcards
+
+
+def mkdir(path):
+    os.mkdir(path)
+
+
+def mkfile(path):
+    open(path, 'w').close()
+
+
+class TestUtils(object):
+
+    def setup_class(cls):
+        cls.tempdir = tempfile.mkdtemp()
+
+        # build the directory structure
+        origin_cwd = os.getcwd()
+        os.chdir(cls.tempdir)
+
+        mkdir('testapi')
+        mkfile('testapi/__init__.py')
+        mkdir('testapi/resources')
+        mkfile('testapi/resources/__init__.py')
+        mkdir('testapi/resources/users')
+        mkfile('testapi/resources/users/__init__.py')
+        mkfile('testapi/resources/users/resource.py')
+        mkdir('testapi/resources/orders')
+        mkfile('testapi/resources/orders/__init__.py')
+        mkfile('testapi/resources/orders/resource.py')
+
+        os.chdir(origin_cwd)
+
+        # add cls.tempdir into sys.path
+        sys.path.insert(0, cls.tempdir)
+
+    def teardown_class(cls):
+        sys.path.remove(cls.tempdir)
+        shutil.rmtree(cls.tempdir)
+
+    def test_load_resources(self):
+        load_resources(['testapi.resources.users.resource'])
+
+    def test_load_resources_with_nonexistent_entrypoints(self):
+        with pytest.raises(ImportError):
+            load_resources(['testapi.resources.counts.resource'])
+
+    def test_load_resources_with_wildcards_entrypoints(self):
+        load_resources(['testapi.resources.*.resource'])
+
+    def test_load_resources_with_nonexistent_wildcards_entrypoints(self):
+        entrypoint = 'testapi.resources.users.*.resource'
+        with pytest.raises(ImportError) as exc:
+            load_resources([entrypoint])
+        expected_exc_msg = 'No module found with wildcards %r' % entrypoint
+        assert str(exc.value) == expected_exc_msg
+
+    def test_expand_wildcards(self):
+        real_entrypoints = expand_wildcards('testapi.resources.*.resource')
+        assert real_entrypoints == ['testapi.resources.users.resource',
+                                    'testapi.resources.orders.resource']
+
+    def test_expand_wildcards_with_nonexistent_entrypoint(self):
+        entrypoints = expand_wildcards('testapi.resources.users.*.resource')
+        assert not entrypoints
