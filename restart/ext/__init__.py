ee06f213ebebe9dbac2ebd72c58bedcaea514b6d
RussellLuo <luopeng.he@gmail.com>
2015-07-01 13:11:28 +0800

    Add support for extension development

---
+++ restart/ext/__init__.py
@@ -0,0 +1,42 @@
+class ExtensionImporter(object):
+    """This importer object redirects imports from ``original_module`` to
+    ``target_module``.
+    More specifically, when a user does ``from restart.ext.foo import bar``,
+    the importer will actually do ``from restart_foo import bar`` instead.
+
+    Inspired by `Flask`.
+    """
+
+    sys = __import__('sys')
+
+    def __init__(self, target_module, original_module):
+        self.target_module = target_module
+        self.original_module = original_module
+        self.prefix = original_module + '.'
+        self.prefix_cutoff = original_module.count('.') + 1
+
+    def install(self):
+        if self not in self.sys.meta_path:
+            self.sys.meta_path.append(self)
+
+    def find_module(self, fullname, path=None):
+        if fullname.startswith(self.prefix):
+            return self
+
+    def load_module(self, fullname):
+        if fullname in self.sys.modules:
+            return self.sys.modules[fullname]
+        modname = fullname.split('.', self.prefix_cutoff)[self.prefix_cutoff]
+        realname = self.target_module % modname
+        module = self.sys.modules[fullname] = __import__(realname)
+        return module
+
+
+# Install the importer
+importer = ExtensionImporter('restart_%s', __name__)
+importer.install()
+
+
+# Delete all symbols
+del importer
+del ExtensionImporter
---
+++ tests/test_ext.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+
+import os
+import sys
+import shutil
+import tempfile
+
+
+def mkdir(path):
+    os.mkdir(path)
+
+
+def mkfile(path, content=''):
+    with open(path, 'w') as f:
+        f.write(content)
+
+
+class TestUtils(object):
+
+    def setup_class(cls):
+        cls.tempdir = tempfile.mkdtemp()
+
+        # build the directory structure
+        origin_cwd = os.getcwd()
+        os.chdir(cls.tempdir)
+
+        # restart extension module
+        mkfile('restart_module.py', 'ext_id = "restart_module"')
+
+        # restart extension package
+        mkdir('restart_package')
+        mkfile('restart_package/__init__.py', 'ext_id = "restart_package"')
+
+        os.chdir(origin_cwd)
+
+        # add cls.tempdir into sys.path
+        sys.path.insert(0, cls.tempdir)
+
+    def teardown_class(cls):
+        sys.path.remove(cls.tempdir)
+        shutil.rmtree(cls.tempdir)
+
+    def test_restart_ext_module_import_normal(self):
+        from restart.ext.module import ext_id
+        assert ext_id == 'restart_module'
+
+    def test_restart_ext_module_import_module(self):
+        from restart.ext import module
+        assert module.ext_id == 'restart_module'
+        assert module.__name__ == 'restart_module'
+
+    def test_restart_ext_package_import_normal(self):
+        from restart.ext.package import ext_id
+        assert ext_id == 'restart_package'
+
+    def test_restart_ext_package_import_module(self):
+        from restart.ext import package
+        assert package.ext_id == 'restart_package'
+        assert package.__name__ == 'restart_package'
