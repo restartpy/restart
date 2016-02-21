17dbc01030d8e78fd3ad0433a1bcd4bd9d789340
RussellLuo <luopeng.he@gmail.com>
2016-02-21 20:59:14 +0800

    Upgrade the Python-EasyConfig dependency

--- restart/config/__init__.py
+++ restart/config/__init__.py
@@ -1,12 +1,11 @@
 from __future__ import absolute_import
 
-from easyconfig import Config, envvar_object
+from easyconfig import Config, str_object
 
 from . import default
 
 
-config = Config()
-config.from_object(default)
+config = Config(default)
 
 # Override the default configuration if `RESTART_CONFIG_MODULE` is given
-config.from_object(envvar_object('RESTART_CONFIG_MODULE', silent=True))
+config.load(str_object('RESTART_CONFIG_MODULE', silent=True, is_envvar=True))
