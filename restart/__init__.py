06c80ff4bacefcb6029add0d605a0d65c7de1111
RussellLuo <luopeng.he@gmail.com>
2019-08-16 12:20:03 +0800

    Bump version to 0.1.6

--- restart/__init__.py
+++ restart/__init__.py
@@ -1,6 +1,6 @@
 """A Python library with good intentions for building REST APIs."""
 
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 __author__ = 'RussellLuo'
 __email__ = 'luopeng.he@gmail.com'
 __license__ = 'MIT'
